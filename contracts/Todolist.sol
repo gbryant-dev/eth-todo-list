4812c423fd3c66f76c8403ea8575c61c31c7caa8
gbryant-dev <gbryant1@hotmail.co.uk>
2019-06-29 17:24:58 +0100

    Toggle completed tasks

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -17,6 +17,11 @@ contract TodoList {
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("Check out my first task");
     }
@@ -27,4 +32,12 @@ contract TodoList {
         emit TaskCreated(taskCount, _content, false);
 
     }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
+
